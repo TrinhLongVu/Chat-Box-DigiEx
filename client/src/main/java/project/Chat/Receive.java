6287ce4c34900262abe883bf75dc715b7135329b
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2024-07-24 08:47:14 +0700

    chore: config receive client & close the socket

--- client/src/main/java/project/Chat/Receive.java
+++ client/src/main/java/project/Chat/Receive.java
@@ -42,83 +42,21 @@ public class Receive extends Thread {
                     System.out.println("Received: " + receiveMsg);
                     TypeReceive data = Helper.FormatData(receiveMsg);
 
-                    switch (data.getType()){
-                        case "online": {
-                            String content = data.getData();
-                            String[] namesArray = content.substring(content.indexOf("[") + 1, content.indexOf("]")).split("\\s*,\\s*");
-                            List<String> namesList = Arrays.asList(namesArray);
-                            DataSave.userOnline = namesList;
-
-                            HomePage.listModelUsers.clear();
-                            for (String user : DataSave.userOnline) {
-                                HomePage.listModelUsers.addElement(user);
-                            }
-
-                            System.out.println("user selected" + DataSave.selectedUser);
+                    switch (data.getType()) {
+                        case "online":
+                            handleOnline(data.getData());
                             break;
-                        }
-                        case "chat": {
-                            String content = data.getData();
-                            String userSend = data.getNameSend();
-                            LinkedList history = DataSave.contentChat.get(userSend);
-                            if(history == null){
-                                history = new LinkedList<>();
-                                DataSave.contentChat.put(userSend, history);
-                            }
-                            history.add(userSend + ": " + content);
-                            if(DataSave.selectedUser.equals(userSend)) {
-                                HomePage.listModel.clear();
-                                for(Object hist: history) {
-                                    HomePage.listModel.addElement((String)hist);
-                                }
-                            }
+                        case "chat":
+                            handleChat(data.getData(), data.getNameSend());
                             break;
-                        }
-                        case "chat-group": {
-                            String content = data.getData();
-                            String userSend[] = data.getNameSend().split(",");
-
-                            LinkedList history = DataSave.contentChat.get(userSend[1]);
-                            if (history == null) {
-                                history = new LinkedList<>();
-                                DataSave.contentChat.put(userSend[1], history);
-                            }
-                            history.add(userSend[0] + ": " + content);
-                            if (DataSave.selectedUser.equals(userSend[1])) {
-                                HomePage.listModel.clear();
-                                for (Object hist : history) {
-                                    HomePage.listModel.addElement((String) hist);
-                                }
-                            }
+                        case "chat-group":
+                            handleChatGroup(data.getData(), data.getNameSend());
                             break;
-                        }
-                        case "server": {
-                            String[] hostAndPort = data.getData().split("@");
-                            System.out.println(hostAndPort[0] + "...." + hostAndPort[1]);
-                        
-                            int port;
-                            String host = hostAndPort[0];
-                        
-                            try {
-                                port = Integer.parseInt(hostAndPort[1]);
-                            } catch (NumberFormatException e) {
-                                System.out.println("Invalid port number format");
-                                return;
-                            }
-                        
-                            Socket s = null;
-                            try {
-                                s = new Socket(host, port);
-                                new Receive(s).start();
-                                new Send(s).sendData("type:login&&send:" + LoginForm.username);
-                                new HomePage(null, s, LoginForm.username);
-                            } catch (IOException e) {
-                                System.out.println("Unable to connect to server: " + e.getMessage());
-                            }
+                        case "server":
+                            handleServer(data.getData());
                             return;
-                        }
                         default:
-                            System.out.println("Received invalid data: " + receiveMsg);
+                            System.out.println("Received invalid data: " + data);
                             break;
                     }
                 }
@@ -127,4 +65,75 @@ public class Receive extends Thread {
             throw new RuntimeException(e);
         }
     }
+    
+
+
+    private void handleOnline(String content) {
+        String[] namesArray = content.substring(content.indexOf("[") + 1, content.indexOf("]")).split("\\s*,\\s*");
+        List<String> namesList = Arrays.asList(namesArray);
+        DataSave.userOnline = namesList;
+
+        HomePage.listModelUsers.clear();
+        for (String user : DataSave.userOnline) {
+            HomePage.listModelUsers.addElement(user);
+        }
+
+        System.out.println("user selected" + DataSave.selectedUser);
+    }
+
+    private void handleChat(String content, String userSend) {
+        LinkedList<String> history = DataSave.contentChat.get(userSend);
+        if (history == null) {
+            history = new LinkedList<>();
+            DataSave.contentChat.put(userSend, history);
+        }
+        history.add(userSend + ": " + content);
+        if (DataSave.selectedUser.equals(userSend)) {
+            HomePage.listModel.clear();
+            for (String hist : history) {
+                HomePage.listModel.addElement(hist);
+            }
+        }
+    }
+
+    private void handleChatGroup(String content, String userSendCombined) {
+        String[] userSend = userSendCombined.split(",");
+        LinkedList<String> history = DataSave.contentChat.get(userSend[1]);
+        if (history == null) {
+            history = new LinkedList<>();
+            DataSave.contentChat.put(userSend[1], history);
+        }
+        history.add(userSend[0] + ": " + content);
+        if (DataSave.selectedUser.equals(userSend[1])) {
+            HomePage.listModel.clear();
+            for (String hist : history) {
+                HomePage.listModel.addElement(hist);
+            }
+        }
+    }
+
+    private void handleServer(String data) {
+        String[] hostAndPort = data.split("@");
+        System.out.println(hostAndPort[0] + "...." + hostAndPort[1]);
+
+        String host = hostAndPort[0];
+        int port;
+        try {
+            port = Integer.parseInt(hostAndPort[1]);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid port number format");
+            return;
+        }
+
+        try {
+            this.socket.close();
+            Socket newSocket = new Socket(host, port);
+            new Receive(newSocket).start();
+            new Send(newSocket).sendData("type:login&&send:" + LoginForm.username);
+            new HomePage(null, newSocket, LoginForm.username);
+        } catch (IOException e) {
+            System.out.println("Unable to connect to server: " + e.getMessage());
+        }
+    }
+
 }
