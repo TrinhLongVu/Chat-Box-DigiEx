0fa584a8e5432293c4df9d2b172222ad8839a51f
Long Vu <vucongleo@gmail.com>
2024-08-20 08:56:42 +0700

    fix: env file

--- server/src/main/java/com/example/Server/Main.java
+++ server/src/main/java/com/example/Server/Main.java
@@ -1,6 +1,5 @@
 package com.example.Server;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
