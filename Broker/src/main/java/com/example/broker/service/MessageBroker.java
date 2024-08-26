791f34c4a13e9064c323dd06973b395070e0c925
Nguyen Huu Phuc <nguyenhuuthuc0405@gmail.com>
2024-08-26 08:48:18 +0700

    Change name format: Broker

--- Broker/src/main/java/com/example/broker/service/MessageBroker.java
+++ Broker/src/main/java/com/example/broker/service/MessageBroker.java
@@ -12,24 +12,26 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.stereotype.Component;
 import java.util.concurrent.CopyOnWriteArrayList;
 import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
-
 
 @Component
 @RequiredArgsConstructor
 public class MessageBroker {
     private ConcurrentHashMap<String, List<Socket>> connectedServers = new ConcurrentHashMap<>();
     private static final Logger log = LogManager.getLogger(MessageBroker.class);
-    private final ServerSocketHandler serverSocketHandler;
+    private final BrokerSocketHandler brokerSocketHandler;
     private static final String SERVER_TOPIC = "Server";
-    public int PORT = 4000;
+
+    @Value("${broker.port}")
+    public int brokerPort;
 
 
     @Bean
     public void init() {
         log.info("MessageBroker is starting...");
         try {
-            serverSocketHandler.setServerSocket(new ServerSocket(PORT));
+            brokerSocketHandler.setBrokerSocket(new ServerSocket(brokerPort));
         } catch (IOException e) {
             log.error("Error starting server: {}", e.getMessage());
         }
@@ -38,12 +40,14 @@ public class MessageBroker {
     @Scheduled(fixedRate = 300)
     private void allowNewServerConnection() {
         try {
-            Socket serverSocket = serverSocketHandler.getServerSocket().accept();
+            Socket serverSocket = brokerSocketHandler.getBrokerSocket().accept();
+
+            // Update value of key present in the map, if the key is not present then add the key and value to the map
             connectedServers.computeIfAbsent(SERVER_TOPIC, _ -> new CopyOnWriteArrayList<>()).add(serverSocket);
 
             new Thread(new ServerHandler(this, serverSocket)).start();
         } catch (IOException e) {
-            log.error("Error accepting connection: {}", e.getMessage());
+            log.error("Error accepting connection from server: {}", e.getMessage());
         }
     }
 
